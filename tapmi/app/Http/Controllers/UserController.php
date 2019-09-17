35b36dad87a24486b66f503a072f1581615df0d5
richardt9golan <apriando.richardt@gmail.com>
2019-09-17 14:27:52 +0700

    URL api ke qa

--- tapmi/app/Http/Controllers/UserController.php
+++ tapmi/app/Http/Controllers/UserController.php
@@ -21,30 +21,33 @@ use URL;
 use App\APISetup;
 use App\APIData as Data;
 
-class UserController extends Controller {
+class UserController extends Controller
+{
 
 	protected $api;
 	protected $active_menu;
 
 	#   		 								  				  ▁ ▂ ▄ ▅ ▆ ▇ █ CONSTRUCTOR
 	# -------------------------------------------------------------------------------------
-	public function __construct() {
+	public function __construct()
+	{
 		$this->url_api_ins_msa_auth = APISetup::url()['msa']['ins']['auth'];
-		$this->active_menu = '_'.str_replace( '.', '', '02.05.00.00.00' ).'_';
+		$this->active_menu = '_' . str_replace('.', '', '02.05.00.00.00') . '_';
 	}
 
 	#   		 								  				        ▁ ▂ ▄ ▅ ▆ ▇ █ INDEX
 	# -------------------------------------------------------------------------------------
-	public function index() {
-		$allowed_role = array( "ADMIN" );
+	public function index()
+	{
+		$allowed_role = array("ADMIN");
 		$data['active_menu'] = $this->active_menu;
 
-		if ( in_array( session('USER_ROLE'), $allowed_role ) ) {
+		if (in_array(session('USER_ROLE'), $allowed_role)) {
 			$data['master_user'] = array();
-			if ( !empty( Data::user_find() ) ) {
+			if (!empty(Data::user_find())) {
 				$i = 0;
-				foreach ( Data::user_find() as $q ) {
-					if ( isset( $q['JOB'] ) && isset( $q['FULLNAME'] ) ) {
+				foreach (Data::user_find() as $q) {
+					if (isset($q['JOB']) && isset($q['FULLNAME'])) {
 						$data['master_user'][$i]['USER_AUTH_CODE'] = $q['USER_AUTH_CODE'];
 						$data['master_user'][$i]['EMPLOYEE_NIK'] = $q['EMPLOYEE_NIK'];
 						$data['master_user'][$i]['USER_ROLE'] = $q['USER_ROLE'];
@@ -56,59 +59,62 @@ class UserController extends Controller {
 						$data['master_user'][$i]['APK_DATE'] = '';
 
 						$client = new \GuzzleHttp\Client();
-						$res = $client->request( 'GET', $this->url_api_ins_msa_auth.'/api/v1.0/server/apk-version/'.$q['USER_AUTH_CODE'], 
+						$res = $client->request(
+							'GET',
+							$this->url_api_ins_msa_auth . '/api/v1.0/server/apk-version/' . $q['USER_AUTH_CODE'],
 							[
 								'headers' => [
-								'Authorization' => 'Bearer '.session( 'ACCESS_TOKEN' )
+									'Authorization' => 'Bearer ' . session('ACCESS_TOKEN')
+								]
 							]
-						]);
-						$x = json_decode( $res->getBody(), true );
+						);
+						$x = json_decode($res->getBody(), true);
 
-						if ( $x['status'] == true ) {
+						if ($x['status'] == true) {
 							$data['master_user'][$i]['APK_VERSION'] = $x['apk_version'];
 						}
 						$i++;
 					}
-					
 				}
 			}
 
-			return view( 'user.index', $data );
+			return view('user.index', $data);
 		}
-		
 	}
 
 	#   		 								  				  ▁ ▂ ▄ ▅ ▆ ▇ █ CREATE USER
 	# -------------------------------------------------------------------------------------
-	public function create() {
-		$allowed_role = array( "ADMIN" );
+	public function create()
+	{
+		$allowed_role = array("ADMIN");
 
-		if ( in_array( session('USER_ROLE'), $allowed_role ) ) {
-			$data['parameter'] = Data::parameter_find( '?PARAMETER_GROUP=USER_ROLE' );
-			return view( 'user.create', $data );
+		if (in_array(session('USER_ROLE'), $allowed_role)) {
+			$data['parameter'] = Data::parameter_find('?PARAMETER_GROUP=USER_ROLE');
+			return view('user.create', $data);
 		}
 	}
 
 	#   		 								  		   ▁ ▂ ▄ ▅ ▆ ▇ █ CREATE USER PROSES
 	# -------------------------------------------------------------------------------------
-	public function create_proses() {
+	public function create_proses()
+	{
 
 		$client = new \GuzzleHttp\Client();
-		$res = $client->request( 'POST', $this->url_api_ins_msa_auth.'/api/user', [
+		$res = $client->request('POST', $this->url_api_ins_msa_auth . '/api/user', [
 			'json' => [
-				'EMPLOYEE_NIK' => Input::get( 'EMPLOYEE_NIK'),
-				'USER_ROLE' => Input::get( 'ROLES'),
-				'USERNAME' => Input::get( 'USERNAME'),
-				'REF_ROLE' => Input::get( 'REFFERENCE_ROLE'),
-				'LOCATION_CODE' => Input::get( 'LOCATION')
+				'EMPLOYEE_NIK' => Input::get('EMPLOYEE_NIK'),
+				'USER_ROLE' => Input::get('ROLES'),
+				'USERNAME' => Input::get('USERNAME'),
+				'REF_ROLE' => Input::get('REFFERENCE_ROLE'),
+				'LOCATION_CODE' => Input::get('LOCATION')
 			],
 			'headers' => [
-				'Authorization' => 'Bearer '.session( 'ACCESS_TOKEN' )
+				'Authorization' => 'Bearer ' . session('ACCESS_TOKEN')
 			]
 		]);
-		$data = json_decode( $res->getBody(), true );
+		$data = json_decode($res->getBody(), true);
 
-		return response()->json( $data );
+		return response()->json($data);
 	}
 
 	#   		 								  				  ▁ ▂ ▄ ▅ ▆ ▇ █ SEARCH USER
@@ -117,21 +123,22 @@ class UserController extends Controller {
 	# Mencari user dari tabel HRIS dan SAP sesuai dengan data yang diinput pada parameter "q".
 	# Max result HRIS dan SAP yang ditampilkan 20.
 	#
-	public function search_user() {
-		
+	public function search_user()
+	{
+
 		$data['total_count'] = 0;
 		$data['items'] = array();
 		$data['incomplete_results'] = false;
-		$url = $this->url_api_ins_msa_auth.'/api/user-search?q='.$_GET['q'];
+		$url = $this->url_api_ins_msa_auth . '/api/user-search?q=' . $_GET['q'];
 
-		if ( isset( $_GET['q'] ) ) {
-			$client = APISetup::ins_rest_client( 'GET', $url );
+		if (isset($_GET['q'])) {
+			$client = APISetup::ins_rest_client('GET', $url);
 			$i = 0;
 
-			if ( $client['status'] == true ) {
-				if ( count( $client['data'] ) > 0 ) {
-					$data['total_count'] = count( $client['data'] );
-					foreach ( $client['data'] as $c ) {
+			if ($client['status'] == true) {
+				if (count($client['data']) > 0) {
+					$data['total_count'] = count($client['data']);
+					foreach ($client['data'] as $c) {
 						$data['items'][$i]['id'] = $c['NIK'];
 						$data['items'][$i]['text'] = $c['NAMA_LENGKAP'];
 						$data['items'][$i]['description'] = $c['JOB_CODE'];
@@ -141,40 +148,41 @@ class UserController extends Controller {
 			}
 		}
 
-		return response()->json( $data );
+		return response()->json($data);
 	}
 
 	#   		 								  				         ▁ ▂ ▄ ▅ ▆ ▇ █ EDIT
 	# -------------------------------------------------------------------------------------
-	public function edit( Request $req ) {
-		$userdata = Data::user_find_one( $req->id );
-		if ( !empty( $userdata['items'] ) ) {
+	public function edit(Request $req)
+	{
+		$userdata = Data::user_find_one($req->id);
+		if (!empty($userdata['items'])) {
 			$data['id'] = $req->id;
 			$data['refrole'] = Data::refrole_find();
-			$data['parameter'] = Data::parameter_find( '?PARAMETER_GROUP=USER_ROLE' );
+			$data['parameter'] = Data::parameter_find('?PARAMETER_GROUP=USER_ROLE');
 			$data['user'] = $userdata['items'];
 
-			return view( 'user.edit', $data );
+			return view('user.edit', $data);
 		}
 	}
 
 	#   		 								  				  ▁ ▂ ▄ ▅ ▆ ▇ █ EDIT PROSES
 	# -------------------------------------------------------------------------------------
-	public function edit_proses( Request $req ) {
+	public function edit_proses(Request $req)
+	{
 		$client = new \GuzzleHttp\Client();
-		$res = $client->request( 'PUT', $this->url_api_ins_msa_auth.'/api/user/'.$req->id, [
+		$res = $client->request('PUT', $this->url_api_ins_msa_auth . '/api/user/' . $req->id, [
 			'json' => [
-				'USER_ROLE' => Input::get( 'USER_ROLE'),
-				'REF_ROLE' => Input::get( 'REFFERENCE_ROLE'),
-				'LOCATION_CODE' => Input::get( 'LOCATION_CODE')
+				'USER_ROLE' => Input::get('USER_ROLE'),
+				'REF_ROLE' => Input::get('REFFERENCE_ROLE'),
+				'LOCATION_CODE' => Input::get('LOCATION_CODE')
 			],
 			'headers' => [
-				'Authorization' => 'Bearer '.session( 'ACCESS_TOKEN' )
+				'Authorization' => 'Bearer ' . session('ACCESS_TOKEN')
 			]
 		]);
-		$data = json_decode( $res->getBody(), true );
+		$data = json_decode($res->getBody(), true);
 
-		return response()->json( $data );
+		return response()->json($data);
 	}
-
 }
