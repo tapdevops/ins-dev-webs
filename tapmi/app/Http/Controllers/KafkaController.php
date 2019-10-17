4b95d7b5ea4219e11161f3282d7cdbe88982ec24
verdomazeg <root@bpmhc.(none)>
2019-10-17 14:13:41 +0700

    Fix Issue Report EBCC Sampling & Compare

--- tapmi/app/Http/Controllers/KafkaController.php
+++ tapmi/app/Http/Controllers/KafkaController.php
@@ -148,9 +148,43 @@ class KafkaController extends Controller
 		
 	}
 
+	# PHP Kafka MOBILE_INSPECTION.TR_FINDING
+	public function RUN_INS_MSA_FINDING_TR_FINDING() {
+		// Kafka Config
+		$topic = "INS_MSA_FINDING_TR_FINDING";
+		$Kafka = new RdKafka\Consumer();
+		# $Kafka->setLogLevel(LOG_DEBUG);
+		$Kafka->addBrokers( config('app.kafkahost') );
+		$Topic = $Kafka->newTopic( $topic );
+		$Topic->consumeStart( 0, RD_KAFKA_OFFSET_BEGINNING );
 
-
-	
+		while ( true ) {
+			$message = $Topic->consume( 0, 1000 );
+			if ( null === $message ) {
+				continue;
+			} 
+			else if ( $message->err ) {
+				echo $message->errstr(), "\n";
+				break;
+			} 
+			else {
+				$payload = json_decode( $message->payload, true );
+				$last_offset = $this->cek_offset_payload( $topic );
+				if ( $last_offset !== false ){
+					if ( $last_offset == null ) {
+						if ( (int)$message->offset >= $last_offset ) {
+							echo $this->insert_tr_finding( $payload, (int)$message->offset );
+						}	
+					}
+					else {
+						if ( (int)$message->offset > $last_offset ) {
+							echo $this->insert_tr_finding( $payload, (int)$message->offset );
+						}	
+					}
+				}
+			}
+		}
+	}
 	
 	public function insert_h( $payload, $offset ) {
 		
@@ -351,6 +385,127 @@ class KafkaController extends Controller
         catch ( \Exception $e ) {
 			return 'Insert Failde: '.$e->getMessage().PHP_EOL;
 		}
+	}
+
+
+
+
+
+// { 
+//    "FNDCD":"F0135191014113236",
+//    "WERKS":"4122",
+//    "AFD_CODE":"O",
+//    "BLOCK_CODE":"379",
+//    "FNDCT":"CA0000001DEW",
+//    "FNDDS":"Kacangan melilit pokok f39 jalur 75/76",
+//    "FNDPR":"MED",
+//    "DUE_DATE":"",
+//    "ASSTO":"0110",
+//    "PRGRS":"0",
+//    "LATFN":"-2.9747407",
+//    "LONFN":"112.3484292",
+//    "RFINC":"",
+//    "INSUR":"0135",
+//    "INSTM":20191014113342,
+//    "UPTUR":"0135",
+//    "UPTTM":20191014113342,
+//    "DLTUR":"",
+//    "DLTTM":0,
+//    "RTGVL":0,
+//    "RTGMS":"",
+//    "END_TIME":0
+// }
+	public function insert_tr_finding( $payload, $offset ) {
+		// $INSTM = ( (bool) strtotime( $payload['INSTM'] ) == true ? "to_date('".date( 'YmdHis', strtotime( $payload['INSTM'] ) )."','YYYYMMDDHH24MISS')" : "NULL" );
+		// $STIME = ( (bool) strtotime( $payload['STIME'] ) == true ? "to_date('".date( 'YmdHis', strtotime( $payload['STIME'] ) )."','YYYYMMDDHH24MISS')" : "NULL" );
+		$check = collect( $this->db_mobile_ins->select( "
+			SELECT 
+				COUNT( * ) AS COUNT 
+			FROM 
+				TR_FINDING
+			WHERE
+				EBCC_VALIDATION_CODE = '{$payload['FNDCD']}'
+		" ) )->first();
+
+		if ( $check->count == 0 ) {
+			print_r( $payload ); print PHP_EOL;
+			/*
+			$sql = "INSERT INTO 
+					MOBILE_INSPECTION.TR_FINDING (
+						FINDING_CODE,
+						WERKS,
+						AFD_CODE,
+						BLOCK_CODE,
+						FINDING_CATEGORY,
+						FINDING_DESC,
+						FINDING_PRIORITY,
+						DUE_DATE,
+						ASSIGN_TO,
+						PROGRESS,
+						LAT_FINDING,
+						LONG_FINDING,
+						REFFERENCE_INS_CODE ,
+						INSERT_USER,
+						INSERT_DATE,
+						UPDATE_USER,
+						UPDATE_TIME,
+						DELETE_USER,
+						DELETE_TIME
+					) 
+				VALUES (
+					'{$payload['URACD']}',
+					'{$payload['EMNIK']}',
+					'{$payload['URROL']}',
+					'{$payload['LOCCD']}',
+					'{$payload['RROLE']}',
+					null,
+					null,
+					null,
+					null,
+					null,
+					null
+			)";
+
+			try {
+				$this->db_mobile_ins->statement( $sql );
+				$this->db_mobile_ins->commit();
+				
+				// Update offset payloads
+				$this->db_mobile_ins->statement( "
+					UPDATE 
+						MOBILE_INSPECTION.TM_KAFKA_PAYLOADS
+					SET
+						OFFSET = $offset,
+						EXECUTE_DATE = SYSDATE
+					WHERE
+						TOPIC_NAME = 'INS_MSA_FINDING_TR_FINDING'
+				" );
+				$this->db_mobile_ins->commit();
+				return date( 'Y-m-d H:i:s' ).' - INS_MSA_FINDING_TR_FINDING - INSERT '.$payload['EBVTC'].' - SUCCESS '.PHP_EOL;
+			}
+			catch ( \Throwable $e ) {
+				return date( 'Y-m-d H:i:s' ).' - INS_MSA_FINDING_TR_FINDING - INSERT '.$payload['EBVTC'].' - FAILED '.$e->getMessage().PHP_EOL;
+	        }
+	        catch ( \Exception $e ) {
+				return date( 'Y-m-d H:i:s' ).' - INS_MSA_FINDING_TR_FINDING - INSERT '.$payload['EBVTC'].' - FAILED '.$e->getMessage().PHP_EOL;
+			}
+			*/
+		}
+		else {
+			// Update offset payloads
+			$this->db_mobile_ins->statement( "
+				UPDATE 
+					MOBILE_INSPECTION.TM_KAFKA_PAYLOADS
+				SET
+					OFFSET = $offset,
+					EXECUTE_DATE = SYSDATE
+				WHERE
+					TOPIC_NAME = 'INS_MSA_FINDING_TR_FINDING'
+			" );
+			$this->db_mobile_ins->commit();
+			return date( 'Y-m-d H:i:s' ).' - INS_MSA_FINDING_TR_FINDING - INSERT '.$payload['EBVTC'].' - DUPLICATE '.PHP_EOL;
+		}
+
 	}
 	
 }
