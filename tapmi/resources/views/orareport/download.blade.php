0129d16b859686b3fa0a63a82df8c7f1cf8d6fb6
Emanuel Riolan <emanuelriolan@gmail.com>
2020-11-18 11:53:29 +0700

    .

--- tapmi/resources/views/orareport/download.blade.php
+++ tapmi/resources/views/orareport/download.blade.php
@@ -36,7 +36,6 @@
 					@if(session('USER_ROLE')=='IT' || session('USER_ROLE')=='ADMIN')
 					<option value="VALIDASI_JANJANG_BY_AI" selected="selected">VALIDASI JANJANG BY AI</option>
 					@endif
-					<option>VALIDASI JANJANG BY AI</option>
 					<option value="EBCC_COMPARE_ESTATE">EBCC COMPARE ESTATE</option>
 					<option value="EBCC_COMPARE_MILL">EBCC COMPARE MILL</option>
 					<option value="EBCC_VALIDATION_ESTATE">EBCC SAMPLING ESTATE</option>
